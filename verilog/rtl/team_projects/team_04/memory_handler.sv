e5fc6fbad5009d95ab60e0ee84f8842c37455c2b
Andy Hu <hu965@ee69lnx35.ecn.purdue.edu>
2024-06-26 11:01:52 -0400

    add default cases to mem handler

--- verilog/rtl/team_projects/team_04/memory_handler.sv
+++ verilog/rtl/team_projects/team_04/memory_handler.sv
@@ -12,55 +12,52 @@ module memory_handler(
 always_comb begin 
     if (en_read) begin 
       mem_read = 1;
+        select = 4'b1111;
+        addr_to_mem = addr;
       mem_write = 0;
       case(size)
         3'b100: begin //lbu
-          addr_to_mem = addr;
-          select = 4'b1111;
           data_to_reg = {24'b0, data_from_mem[7:0]};
         end
         3'b000: begin //lb
-          addr_to_mem = addr;
-          select = 4'b1111;
           data_to_reg = {{24{data_from_mem[7]}}, data_from_mem[7:0]};
         end
         3'b101: begin //lhu
-          addr_to_mem = addr;
-          select = 4'b1111;
           data_to_reg = {16'b0, data_from_mem[15:0]};
         end
         3'b001: begin //lh
-          addr_to_mem = addr;
-          select = 4'b1111;
           data_to_reg = {{16{data_from_mem[16]}}, data_from_mem[15:0]};
         end
         3'b010: begin //lw
-          addr_to_mem = addr;
-          select = 4'b1111;
           data_to_reg = data_from_mem;
         end
+        default:
+          data_to_reg = 0;
       endcase
     end
 
     if (en_write) begin 
       mem_read = 0;
       mem_write = 1;
+      addr_to_mem = addr;
       case(size) 
         3'b000: begin //sb 
-          addr_to_mem = addr;
           data_to_mem[7:0] = read_data_2[7:0];
           select = 4'b0001;
         end
         3'b001: begin //sh
-          addr_to_mem = addr;
           data_to_mem[15:0] = read_data_2[15:0];
           select = 4'b0011;
         end
         3'b010: begin //sw
-          addr_to_mem = addr;
           data_to_mem = read_data_2;
           select = 4'b1111;
         end
+        default: begin
+          data_to_mem = 0;
+          select = 0;
+        end
+        
       endcase  
     end
 end
