7778877de204bf65ed252779e1f90e63a6f8b993
JoseLS13 <sierra6@purdue.edu>
2024-07-05 01:41:19 +0000

    rx_update

--- verilog/rtl/team_projects/team_11/UART_top_module.sv
+++ verilog/rtl/team_projects/team_11/UART_top_module.sv
@@ -1,5 +1,5 @@
 module top (
-  input logic clk, rst, rx_din,
+  input logic clk, nrst, rx_din,
   output logic success, receive_ready,
   output logic [7:0]data_receive, 
   output logic [8:0]show_internal,
@@ -12,20 +12,19 @@ module top (
     logic no_count;
     logic send;
 
-    second_counter secondcount(.clk(clk), .rst(rst), .falling_edge(falling_edge), .max(4'b1001),  .no_count(no_count), .countout());
+    second_counter secondcount(.clk(clk), .nrst(nrst), .falling_edge(falling_edge), .max(4'b1001),  .no_count(no_count), .countout());
 
-    shift_register shifting_in(.clk(clk), .rst(rst), .Reg_Start(Reg_Start), .receive_ready(send), .Din(rx_din), .data_receive(data_receive), .show_internal(show_internal));
+    shift_register shifting_in(.clk(clk), .nrst(nrst), .Reg_Start(Reg_Start), .receive_ready(send), .Din(rx_din), .data_receive(data_receive), .show_internal(show_internal));
 
-    counter count(.clk(clk), .rst(rst), .count_start(Count_Start), .max(4'b1001), .atmax(receive_ready));
+    counter count(.clk(clk), .nrst(nrst), .count_start(Count_Start), .max(4'b1001), .atmax(receive_ready));
     
-    FSM test(.clk(clk), .rst(rst), .receive_ready(receive_ready), .falling_edge(falling_edge),
+    FSM test(.clk(clk), .nrst(nrst), .receive_ready(receive_ready), .falling_edge(falling_edge),
     .state_check(), .Success(success), .Reg_Start(Reg_Start), .Count_Start(Count_Start), .no_count(no_count), .send(send));
 
-    clock_divider baud(.clk(clk), .rst(rst), .new_clk(new_clk));
+    clock_divider baud(.clk(clk), .nrst(nrst), .new_clk(new_clk));
 
-    edge_detect detect(.clk(clk), .rst(rst), .signal_in(rx_din), .falling_edge(falling_edge));
+    edge_detect detect(.clk(clk), .nrst(nrst), .signal_in(rx_din), .falling_edge(falling_edge));
 
-    sr_rx rx_register(.clk(clk), .rst(rst), .receive_ready(send), .data_receive(data_receive), .msg(msg));
+    sr_rx rx_register(.clk(clk), .nrst(nrst), .receive_ready(send), .data_receive(data_receive), .msg(msg));
 
 endmodule
-
