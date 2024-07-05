fb757ce225a91b4040249a3b1aab5cf306c50967
JoseLS13 <sierra6@purdue.edu>
2024-07-05 00:35:16 +0000

    rx_update

--- verilog/rtl/team_projects/team_11/UART_top_module.sv
+++ verilog/rtl/team_projects/team_11/UART_top_module.sv
@@ -1,27 +1,31 @@
 module top (
-  input logic clk, rst, rx_din, rx_start,
+  input logic clk, rst, rx_din,
   output logic success, receive_ready,
-  output logic [7:0]data_receive, show_internal
+  output logic [7:0]data_receive, 
+  output logic [8:0]show_internal,
+  output [127:0] msg
 );
 
-    //INCOMING DATA PACKET IS 1'b1,1'b0,data8bits,1'b0;
     logic Reg_Start, Count_Start, new_clk;
-    logic [7:0]din, falling_edge;
+    logic falling_edge;
     logic Ready;
     logic no_count;
+    logic send;
 
-    second_counter secondcount(.clk(clk), .rst(rst), .falling_edge(falling_edge), .max(4'b0111),  .no_count(no_count), .countout());
+    second_counter secondcount(.clk(clk), .rst(rst), .falling_edge(falling_edge), .max(4'b1010),  .no_count(no_count), .countout());
 
-    shift_register shifting_in(.clk(clk), .rst(rst), .Reg_Start(Reg_Start), .Ready(receive_ready), .Din(rx_din), .no_count(no_count), .data_receive(data_receive), .show_internal(show_internal));
+    shift_register shifting_in(.clk(clk), .rst(rst), .Reg_Start(Reg_Start), .receive_ready(send), .Din(rx_din), .data_receive(data_receive), .show_internal(show_internal));
 
-    counter count(.clk(clk), .rst(rst), .count_start(Count_Start), .falling_edge(falling_edge), .max(4'b0111), .atmax(receive_ready), .no_count(no_count));
+    counter count(.clk(clk), .rst(rst), .count_start(Count_Start), .max(4'b1001), .atmax(receive_ready));
     
-    FSM test(.clk(clk), .rst(rst), .Start(rx_start), .Ready_Sig(receive_ready),
-    .state_check(), .Success(success), .Reg_Start(Reg_Start), .Count_Start(Count_Start), .no_count(no_count));
+    FSM test(.clk(clk), .rst(rst), .receive_ready(receive_ready), .falling_edge(falling_edge),
+    .state_check(), .Success(success), .Reg_Start(Reg_Start), .Count_Start(Count_Start), .no_count(no_count), .send(send));
 
     clock_divider baud(.clk(clk), .rst(rst), .new_clk(new_clk));
 
-    edge_detect detect(.clk(clk), .rst(rst), .signal_in(rx_din), .Ready(receive_ready), .falling_edge(falling_edge));
+    edge_detect detect(.clk(clk), .rst(rst), .signal_in(rx_din), .falling_edge(falling_edge));
+
+    sr_rx rx_register(.clk(clk), .rst(rst), .receive_ready(send), .data_receive(data_receive), .msg(msg));
 
 endmodule
 
