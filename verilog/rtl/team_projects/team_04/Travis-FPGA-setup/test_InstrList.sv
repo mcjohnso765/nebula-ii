bc8a5a3f6c8a4ddb61c46c9d7e76e6713ec74179
tjdjakl <148018180+tjdjakl@users.noreply.github.com>
2024-07-08 14:16:38 -0400

    Update test_InstrList.sv

--- verilog/rtl/team_projects/team_04/Travis-FPGA-setup/test_InstrList.sv
+++ verilog/rtl/team_projects/team_04/Travis-FPGA-setup/test_InstrList.sv
@@ -19,11 +19,11 @@ li x14,  0x60000000
 li x15,  0x40000000
 li x16,  0x70000000
 
+addi x17, x0, 2        /* Gun location */
+
 _center:
     sw x2, 0(x1) 
-    sw x2, -4(x1) 
-    sw x2, 4(x1) 
-    
+	sw x17, -4(x1)
 
 _check_button:
     addi x5, x0, 2     /* Key code for 'up' button */
@@ -39,21 +39,17 @@ _check_button:
     j _check_button
 
 _move_up:
-	sw x0, -5(x1)
+
     sw x0, -4(x1)           /* clear current pixel */
-    sw x0, -3(x1)
-    sw x0, -1(x1)
+
     sw x0, 0(x1)           /* clear current pixel */
-    sw x0, 1(x1)
-    sw x0, 3(x1)
+
     sw x0, 4(x1)           /* clear current pixel */
-    sw x0, 5(x1)
+
     
     addi x1, x1, -4        /* update memory location (move up)*/
     blt x1, x0, _move_up_wrap
     sw x2, 0(x1)           /* fill screen */
-    sw x2, -4(x1)           /* fill screen */
-    sw x2, 4(x1)           /* fill screen */
     j _delay_loop
     
 _move_up_wrap:
@@ -62,134 +58,96 @@ _move_up_wrap:
     j _delay_loop
 
 _move_down:
-    sw x0, -5(x1)
-    sw x0, -4(x1)           /* clear current pixel */
-    sw x0, -3(x1)
-    sw x0, -1(x1)
     sw x0, 0(x1)           /* clear current pixel */
-    sw x0, 1(x1)
-    sw x0, 3(x1)
-    sw x0, 4(x1)           /* clear current pixel */
-    sw x0, 5(x1)          /* clear current pixel */
+
     addi x1, x1, 4         /* update memory location (move down) */
     bge x1, x3, _move_down_wrap
     sw x2, 0(x1)           /* fill screen */
-    sw x2, -4(x1)           /* fill screen */
-    sw x2, 4(x1)           /* fill screen */
+
     j _delay_loop
     
 _move_down_wrap:
 	addi x1, x1, -384
     sw x2, 0(x1)           /* fill screen */
-    sw x2, 4(x1)           /* fill screen */
-    sw x2, 384(x1)           /* fill screen */
     j _delay_loop
 
 _move_left:
-    sw x0, -5(x1)
-    sw x0, -4(x1)           /* clear current pixel */
-    sw x0, -3(x1)
-    sw x0, -1(x1)
     sw x0, 0(x1)           /* clear current pixel */
-    sw x0, 1(x1)
-    sw x0, 3(x1)
-    sw x0, 4(x1)           /* clear current pixel */
-    sw x0, 5(x1)           /* clear current pixel */
+    sw x0, 1(x1)           /* clear current pixel */
+    sw x0, -4(x1)           /* clear current pixel */
     beq x2, x15, _wrap_left
     beq x2, x14, _two_bit_left /* BEQ = 0x60000000 */
     beq x2, x16, _one_bit_left /* BEQ = 0x70000000 */
     slli x2, x2, 1         /* shift pixel left */
+    slli x17, x17, 1
     bnez x2, _no_wrap_left /* if not zero, continue */
-    j _delay_loop
     
 _wrap_left:
     addi x1, x1, -1        /* move to previous memory location */
     li x2, 0x7         /* set pixel to LSB */
+    slli x17, x17, 1
     sw x2, 0(x1)           /* set new pixel */
-    sw x2, -4(x1)           /* set new pixel */
-    sw x2, 4(x1)           /* set new pixel */
+    sw x17, -4(x1)           /* set new gun */
     j _delay_loop
     
 _no_wrap_left:
     sw x2, 0(x1)           /* set new pixel */
-    sw x2, -4(x1)           /* set new pixel */
-    sw x2, 4(x1)           /* set new pixel */
+    sw x17, -4(x1)           /* set new gun */
     j _delay_loop
     
 _one_bit_left:
 	li x2, 0x1
     sw x2, -1(x1)
-    sw x2, -5(x1)
-    sw x2, 3(x1)
     li x2, 0x60000000
     sw x2, 0(x1)
-    sw x2, -4(x1)           /* set new pixel */
-    sw x2, 4(x1)           /* set new pixel */
+    sw x17, -4(x1)           /* set new gun */
 	j _delay_loop
     
 _two_bit_left:
 	li x2, 0x3
     sw x2, -1(x1)
-    sw x2, -5(x1)
-    sw x2, 3(x1)
     li x2, 0x40000000  
+    li x17, 0x1
     sw x2, 0(x1)
-    sw x2, -4(x1)           /* set new pixel */
-    sw x2, 4(x1)           /* set new pixel */
+    sw x17, -5(x1)           /* set new gun */
 	j _delay_loop
 
 _move_right:
-    sw x0, -5(x1)
-    sw x0, -4(x1)           /* clear current pixel */
-    sw x0, -3(x1)
-    sw x0, -1(x1)
     sw x0, 0(x1)           /* clear current pixel */
-    sw x0, 1(x1)
-    sw x0, 3(x1)
-    sw x0, 4(x1)           /* clear current pixel */
-    sw x0, 5(x1)
+    sw x0, -1(x1)
 
     srli x2, x2, 1         /* shift pixel right */
     beq x2, x0, _wrap_right
     beq x2, x11, _two_bit_right /* BEQ = 0x1 */
     beq x2, x13, _one_bit_right /* BEQ = 0x3 */
     bnez x2, _no_wrap_right /* if not zero, continue */
-    j _delay_loop
     
 _wrap_right:
 	addi x1, x1, 1         /* move to next memory location */
     li x2, 0x70000000      /* set pixel to MSB */
     sw x2, 0(x1)
-    sw x2, -4(x1)           /* set new pixel */
-    sw x2, 4(x1)           /* set new pixel */
+    
     j _delay_loop
     
 _no_wrap_right:
     sw x2, 0(x1)           /* set new pixel */
-    sw x2, -4(x1)           /* set new pixel */
-    sw x2, 4(x1)           /* set new pixel */
     j _delay_loop
     
 _one_bit_right:
 	li x2, 0x40000000
     sw x2, 1(x1)
-    sw x2, 5(x1)
-    sw x2, -3(x1)
     li x2, 0x3
     sw x2, 0(x1)
-    sw x2, -4(x1)           /* set new pixel */
-    sw x2, 4(x1)           /* set new pixel */
+    
 	j _delay_loop
     
 _two_bit_right:
 	li x2, 0x60000000      
     sw x2, 1(x1)
-    sw x2, 5(x1)
-    sw x2, -3(x1)
-    li x2, 0x1      
+    li x2, 0x1     
     sw x2, 0(x1)
-    sw x2, -4(x1)           /* set new pixel */
-    sw x2, 4(x1)           /* set new pixel */
+    
+    
 	j _delay_loop
 
 _delay_loop:
