47d9c8ea9b6f421601e677c8ad43cf1d0d8607c2
maryjoanfrank <157513323+maryjoanfrank@users.noreply.github.com>
2024-07-08 08:19:40 -0400

    ifdef not ifndef

--- verilog/dv/team_04/module_tests/tippy_top_tb.sv
+++ verilog/dv/team_04/module_tests/tippy_top_tb.sv
@@ -212,7 +212,7 @@ module ram ( //og gonna change this up
 );
 
 logic [31:0] mem [4095:0];
- `ifndef SYNTHESIS
+ `ifdef SYNTHESIS
 initial $readmemh("cpu.mem", mem, 0, 4095);
     `endif
 always_ff @(posedge clk) begin
