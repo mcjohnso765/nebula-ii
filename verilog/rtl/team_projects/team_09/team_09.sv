06a5a36a9297444f0918df4cf19a18a38e7709dd
Jash Pola <jpola@purdue.edu>
2024-07-09 11:31:33 -0400

    fixed more stuff

--- verilog/rtl/team_projects/team_09/team_09.sv
+++ verilog/rtl/team_projects/team_09/team_09.sv
@@ -31,8 +31,8 @@ module team_09 (
 
     // All outputs must have a value even if not used
     assign la_data_out = 128'b0;
-    assign gpio_out = 34'b0; //Inputs, but set low anyways
-    assign gpio_oeb = '1;//All 1's inputs
+    // assign gpio_out = 34'b0; //Inputs, but set low anyways
+    assign gpio_oeb = 34'0011111110000000000000000000000000;//All 1's inputs
     /*
     * Place code and sub-module instantiations here.
     */
