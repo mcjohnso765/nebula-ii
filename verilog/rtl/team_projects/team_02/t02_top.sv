e6e4f02a018e9e8973457968f1e5ef9477f64913
Daniel <danielkimcho@gmail.com>
2024-07-30 22:23:23 +0000

    updated top

--- verilog/rtl/team_projects/team_02/t02_top.sv
+++ verilog/rtl/team_projects/team_02/t02_top.sv
@@ -6,13 +6,12 @@ module t02_top (
 	input logic clk, nrst, enable,
   output logic [31:0] ramaddr, ramstore, 
   output logic Ren, Wen, 
-  output logic [3:0] sel_i,
-  input logic [31:0] ramload, 
-  input logic busy_o
-  // output logic lcd_en, lcd_rw, lcd_rs,
-  // output logic [7:0] lcd_data,
-  // input logic [3:0] read_row,
-  // output logic [3:0] scan_col
+  input logic [31:0] ramload,
+  input logic busy_o,
+  output logic lcd_en, lcd_rw, lcd_rs,
+  output logic [7:0] lcd_data,
+  input logic [3:0] read_row,
+  output logic [3:0] scan_col
 
   // output logic [31:0][31:0] test_memory ,
   // output logic [31:0][31:0] test_nxt_memory 
@@ -23,15 +22,30 @@ module t02_top (
  logic [5:0] cuOP;
  logic [19:0] imm;
  logic [31:0] memload, aluIn, aluOut, immOut, pc, writeData, regData1, regData2, instruction;
+ logic [127:0] row1, row2;
+ logic [15:0] halfData;
+ logic [31:0] FPGAAdress, FPGADataOut;
+ logic FPGAEnable, writeFPGA, CPUEnable;
+ logic keyStrobe, enData, nrstFPGA, write_enable;
 
+ t02_fpgaModule a1 (.clk(clk), .nrst(nrst), .instruction(instruction), .dataIn(memload), .buttons(read_row), .FPGAEnable(FPGAEnable), .writeFPGA(writeFPGA), .CPUEnable(CPUEnable), .address(FPGAAdress), 
+ .dataOut(FPGADataOut), .writeData(writeData), .nrstFPGA(nrstFPGA), .row1(row1), .row2(row2), .keyStrobe(keyStrobe), 
+ .halfData(halfData), .enData(enData));
 
+logic [31:0] muxxedAddressOut, muxxedDataOut;
+logic [31:0]intermedWriteEnable;
+
+t02_mux enableWrite(.in1({31'b0, writeFPGA}), .in2({31'b0, memWrite}), .en(FPGAEnable), .out(intermedWriteEnable));
+t02_mux enableFpgaData(.in1(FPGADataOut), .in2(regData2), .en(FPGAEnable), .out(muxxedDataOut));
+t02_mux enableFpgaAddress(.in1(FPGAAdress), .in2(aluOut), .en(FPGAEnable), .out(muxxedAddressOut));
+assign write_enable = intermedWriteEnable[0];
 
 t02_mux aluMux(.in1(immOut), .in2(regData2), .en(aluSrc), .out(aluIn));
 
 t02_alu arith(.aluOP(aluOP), .inputA(regData1), .inputB(aluIn), .ALUResult(aluOut), .zero(zero), .negative(negative));
 
 t02_register_file DUT(.clk(clk), .nRST(nrst), .reg_write(regWrite), .read_index1(regsel1), .read_index2(regsel2), 
-.read_data1(regData1), .read_data2(regData2), .write_index(w_reg), .write_data(writeData), .en(enable));
+.read_data1(regData1), .read_data2(regData2), .write_index(w_reg), .write_data(writeData));
 
 t02_control controller (.cuOP(cuOP), .instruction(instruction), 
 .reg_1(regsel1), .reg_2(regsel2), .rd(w_reg),
@@ -43,22 +57,12 @@ t02_writeToReg write(.cuOP(cuOP), .memload(memload), .aluOut(aluOut), .imm(immOu
 
 t02_signExtender signex(.imm(imm), .immOut(immOut), .CUOp(cuOP));
 
-//t02_request ru(.CLK(clk), .nRST(nrst), .imemload(instruction), .imemaddr(pc), .dmmaddr(aluOut), .dmmstore(regData2), .ramaddr(ramaddr), .ramload(ramload), .ramstore(ramstore), 
-//.cuOP(cuOP), .Wen(Wen), .busy_o(busy_o), .dmmload(memload), .i_ready(i_ready), .d_ready(d_ready),.Ren(Ren), .en(enable));
+t02_new_request_unit ru(.CLK(clk), .nRST(nrst), .imemload(instruction), .imemaddr(pc), .dmmaddr(aluOut), .dmmstore(regData2), .ramaddr(ramaddr), .ramload(ramload), .ramstore(ramstore), 
+.cuOP(cuOP), .Wen(Wen), .busy_o(busy_o), .dmmload(memload), .i_ready(i_ready), .d_ready(d_ready),.Ren(Ren), .en(enable));
 
-t02_new_request_unit ru(.CLK(clk), .nRST(nrst), .DataAddress(aluOut), .InstrAddress(pc), .DatatoWrite(regData2),
-.iready(i_ready), .dready(d_ready), 
-.FetchedData(memload), .FetchedInstr(instruction), 
-.cuOP(cuOP), .
-busy_o(busy_o), 
-.cpu_dat_o(ramload), 
-.write_i(Wen), .read_i(Ren),
-.adr_i(ramaddr), .cpu_data_i(ramstore),
-.sel_i(sel_i)
-);
-// t02_edgeDetector edg2(.clk(clk), .nRst_i(nrst), .button_i(~keyStrobe), .button_p(enData));
-// t02_keypad pad (.clk(clk), .rst(nrst), .receive_ready(keyStrobe), .data_received(halfData), .read_row(read_row), .scan_col(scan_col));
-// t02_lcd1602 lcd (.clk(clk), .rst(nrst), .row_1(row1), .row_2(row2), .lcd_en(lcd_en), .lcd_rw(lcd_rw), .lcd_rs(lcd_rs), .lcd_data(lcd_data));
+t02_edgeDetector edg2(.clk(clk), .nRst_i(nrst), .button_i(~keyStrobe), .button_p(enData));
+t02_keypad pad (.clk(clk), .rst(nrst), .receive_ready(keyStrobe), .data_received(halfData), .read_row(read_row), .scan_col(scan_col));
+t02_lcd1602 lcd (.clk(clk), .rst(nrst), .row_1(row1), .row_2(row2), .lcd_en(lcd_en), .lcd_rw(lcd_rw), .lcd_rs(lcd_rs), .lcd_data(lcd_data));
 
 //ru_ram rram (.clk(clk), .nRst(nrst), .write_enable(write_enable), .addr(addr), .data_in(datain), .data_out(dataout), .busy(busy_o));
 //ram ra(.clk(clk), .nRst(nrst), .write_enable(memWrite), .read_enable(1), .address_DM(aluOut[5:0]), .address_IM(pc[5:0]), .data_in(regData2), .data_out(memload), .instr_out(instruction), .pc_enable(i_ready), .CUOp(cuOP));
