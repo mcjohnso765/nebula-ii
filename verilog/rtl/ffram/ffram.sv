aa491ac4a2742d215bce44f1076a8b5c059d7656
Miguel Isrrael Teran <misrrael@purdue.edu>
2024-09-01 20:10:16 -0400

    Add wishbone wrapper for FFRAM

--- verilog/rtl/ffram/ffram.sv
+++ verilog/rtl/ffram/ffram.sv
@@ -1,19 +1,22 @@
 module ffram #(
-    parameter WORD_NUM = 256, 
-    parameter WORD_W = 32
-    parameter AD_SPACE = $clog2(WORD_NUM))
+    parameter WORD_NUM = 256, //number of words
+    parameter WORD_W = 32, //word width in bits
+    parameter AD_WIDTH = $clog2(WORD_NUM) //address width (word addressable)
+)
 (
     input  logic [WORD_W-1:0] d_in,     //data in
     input  logic [WORD_W-1:0] bit_en,   //bit enable, active high
-    input  logic [WORD_W-1:0] addr,     //address
-    input  logic              r_wb,     //synchronous wb reset (active high I think)
+    input  logic [AD_WIDTH-1:0] addr,     //word address
+    input  logic              rst,     //synchronous active high reset
     input  logic              clk,      //clk
-    input  logic              en,       //I decided to implement this as "read not write"
+    input  logic              wb_en,    // enable from WB
+    input  logic              r_en,       //read enable
     output logic [WORD_W-1:0] d_out     //data out
 );
 
     logic [WORD_NUM-1:0][WORD_W-1:0] mstate, mstate_n;
     logic [WORD_W-1:0] d_out_n;
+
     always_ff @ (posedge clk) begin
         mstate <= mstate_n;
         d_out <= d_out_n;
@@ -23,13 +26,15 @@ module ffram #(
         mstate_n = mstate;
         d_out_n = d_out;
 
-        if(~en) begin
-            mstate_n[addr[AD_SPACE-1:0]] = d_in;
-        end else begin
-            d_out_n = bit_en & mstate[addr[AD_SPACE-1:0]];
+        if (wb_en) begin
+            if(~r_en) begin
+                mstate_n[addr] = bit_en & d_in;
+            end else begin
+                d_out_n = bit_en & mstate[addr];
+            end
         end
 
-        if(r_wb) begin
+        if(rst) begin
             mstate_n = '0;
             d_out_n = '0;
         end
